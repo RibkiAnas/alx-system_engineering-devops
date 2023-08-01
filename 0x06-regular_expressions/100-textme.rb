bdc3d08cf4912ba3b69820f9ab7ffdc34e4698b1
Anas Ribki <anasribki147@gmail.com>
2023-08-01 22:02:12 +0100

    Edit task 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.\d*?)\] \[to:(.\d*?)\] \[flags:(.\d*?)\]/).join(',')
+puts ARGV[0].scan(/\[from:(.*?|\b*?)\] \[to:(.\d*?|\b*?)\] \[flags:(.\d*?|\b*?)\]/).join(',')
