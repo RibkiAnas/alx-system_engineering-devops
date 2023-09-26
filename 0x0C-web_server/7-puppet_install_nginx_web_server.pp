6d6004a1e9791fff466cce93ddc2cdf873bad245
Anas Ribki <anasribki147@gmail.com>
2023-09-26 17:59:45 +0100

    Edit task 5. Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,30 @@
 # Install and configure nginx server using puppet
 
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
-}
-
+# Ensure nginx package is installed
 package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['apt-get-update'],
+  ensure => 'present',
 }
 
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  require => Package['nginx'],
+# Update system and install nginx
+exec { 'install_nginx':
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx',
+  provider => 'shell',
 }
 
-exec { 'redirect_me':
-  command  => 'sed -i "24i\	rewrite^\/redirect_me https:\/\/www.linkedin.com\/in\/anas-ribki permanent;"
-  /etc/nginx/sites-available/default',
-  provider => 'shell'
+# Create a simple "Hello World!" index.html file
+exec { 'create_index_html':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => 'shell',
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+# Add a redirect rule to the default nginx configuration
+exec { 'add_redirect_rule':
+  command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/linkedin.com\/in\/anas-ribki;\\n\\t}/" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+# Restart the nginx service to apply changes
+exec { 'restart_nginx':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
 }
