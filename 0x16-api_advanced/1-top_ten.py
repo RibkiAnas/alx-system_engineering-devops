c3983ba9f71ff4bb4ddd8766d6948e60a65be604
Anas Ribki <anasribki147@gmail.com>
2023-11-07 19:31:35 +0100

    Add task 1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Titles of the first 10 hot posts listed for
+a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    headers = {
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0"
+            }
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+
+    res = requests.get(url, headers=headers)
+
+    if res.status_code == 200:
+        data = res.json()
+        posts = data["data"]["children"]
+
+        for i in range(10):
+            title = posts[i]["data"]["title"]
+            print(title)
+    else:
+        print(None)
